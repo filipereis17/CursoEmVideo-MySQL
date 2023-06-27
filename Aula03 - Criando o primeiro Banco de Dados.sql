7c815459ab237c79e7cee858c5b4d3700b5c0652
Filipe Reis <filipe.reis17@live.com>
2023-06-27 13:51:27 -0400

    Aula03

---
+++ Aula03 - Criando o primeiro Banco de Dados.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE cadastro;
+
+USE cadastro; 
+
+CREATE TABLE pessoas (
+nome varchar(30),
+idade tinyint,
+sexo char(1),
+peso float,
+altura float,
+nacionalidade varchar(20)
+);
+
+DESCRIBE pessoas;
+SHOW TABLES;
