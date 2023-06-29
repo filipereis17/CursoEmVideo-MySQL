e519779f2d0b0a77cc345334831541ee8220c81c
Filipe Reis <filipe.reis17@live.com>
2023-06-29 09:48:06 -0400

    Aula09

---
+++ Aula09 - PHPMyAdmin (Parte 1).sql
@@ -0,0 +1,16 @@
+/* Usar o MySQL no terminal (shell)
+1 mysql -u <nome de usuário*> -p 			--Exemplo root
+2 Digitar Senha 									--Exemplo: <vazia>
+*/
+
+-- Comandos
+SHOW DATABASES;
+-- STATUS
+USE cadastro;
+SHOW TABLES;
+DESC cursos;
+
+SELECT * FROM cursos;
+
+-- PHPMyAdmin
+-- No navegador digitar localhost e em seguida PHPMyAdmin
